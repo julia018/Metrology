6895a9177deea20cc2006eb19af80edc5e619fe3
YanaDubov <dubov@gmail.com>
2018-10-06 19:54:24 +0300

    with list

--- Unit1.dfm
+++ Unit1.dfm
@@ -11,6 +11,7 @@ object Form1: TForm1
   Font.Name = 'MS Sans Serif'
   Font.Style = []
   OldCreateOrder = False
+  OnCreate = FormCreate
   PixelsPerInch = 96
   TextHeight = 13
   object lbl1: TLabel
--- Unit1.pas
+++ Unit1.pas
@@ -12,17 +12,38 @@ type
     lbl2: TLabel;
     btnopen: TButton;
     strngrd1: TStringGrid;
+    procedure FormCreate(Sender: TObject);
   private
     { Private declarations }
   public
     { Public declarations }
   end;
 
+type
+  Adr = ^List;
+  //Список операторов\операндов
+  List = record
+    Oper: string[255];
+    Repeating: Integer;
+    Next: Adr;
+    end;
+
 var
   Form1: TForm1;
 
+  Operator: Adr;  //Адрес начала списка операторов
+  Operand: Adr;   //Адрес начала списка операндов
 implementation
 
 {$R *.dfm}
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  //Создание двух списков: операндов и операторов
+  New(Operator);
+  Operator.Next:= nil;
+  New(Operand);
+  Operand.Next := nil
+end;
 
 end.
